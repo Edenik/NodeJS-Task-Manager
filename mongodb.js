2fdad6f1ee341c630a1fbba44bce28d08a57db24
Edenik <Edenik5@gmail.com>
2020-12-10 23:41:00 +0200

    Inserting Documents

--- mongodb.js
+++ mongodb.js
@@ -13,8 +13,50 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name:'Eden',
-        age:25
+    // db.collection('users').insertOne({
+    //     name:'Esdasdsaden',
+    //     age:25
+    // }, (error , result) => {
+    //     if(error){
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // var array = [{
+    //     name:'alon',
+    //     age:26
+    // }, {
+    //     name:'daniel',
+    //     age:24
+    // }];
+    // db.collection('users').insertMany(array, (error,result) => {
+    //     if(error){
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    var tasks = [{
+        description:'buy shoes',
+        completed:false,
+    },{
+        description:'complete this course',
+        completed:false,
+    },{
+        description:'get a new job',
+        completed:false,
+    }]
+
+    db.collection('tasks').insertMany(tasks,(error,result) => {
+        if(error){
+            return console.log('Unable to add tasks')
+        }
+        
+        console.log(result.ops)
     })
+
 })
+
