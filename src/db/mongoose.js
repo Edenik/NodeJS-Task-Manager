a049f819c2132ac7837725589c268ad872e73ae4
Edenik <Edenik5@gmail.com>
2020-12-11 14:24:24 +0200

    Data Validation and Sanitization: Part I

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,49 +1,70 @@
 const mongoose = require('mongoose')
-
+const validator = require('validator')
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
 mongoose.connect(connectionURL, {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useCreateIndex: true,
 })
 
-const User = mongoose.model('User' ,{
+const User = mongoose.model('User', {
     name: {
         type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type:Number,
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be positive number')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name:"Alon",
-//     age:27
-// })
+const me = new User({
+    name: "   John Doe           ",
+    email: ' John@gmail.com    ',
 
-// me.save().then((me) =>{
-//     console.log(me)
-// }).catch((error)=> {
-//     console.log('Error!' , error)
-// })
-
-const Task = mongoose.model('Task' , {
-    description : {
-        type:String
-    }, 
-    completed: {
-        type:Boolean
-    }
 })
 
-
-const newTask = new Task({
-    description : "Get a job!",
-    completed:false
-})
-
-newTask.save().then(() => {
-    console.log(newTask)
+me.save().then((me) => {
+    console.log(me)
 }).catch((error) => {
     console.log('Error!', error)
 })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    }
+})
+
+
+// const newTask = new Task({
+//     description : "Get a job!",
+// })
+
+// newTask.save().then(() => {
+//     console.log(newTask)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
