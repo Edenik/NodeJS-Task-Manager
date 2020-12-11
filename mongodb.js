67db853c1f394d77179910a8ec296512f7cb8076
Edenik <Edenik5@gmail.com>
2020-12-11 13:43:02 +0200

    Deleting Documents

--- mongodb.js
+++ mongodb.js
@@ -12,6 +12,37 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
+    
+    // ********Create********
+
+    // db.collection('users').insertOne({
+    //     name:"John Doe",
+    //     age: 30
+    // }).then((result) =>{
+    //     console.log(result.insertedCount)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // const users = [{
+    //     name:"Ayala",
+    //     age: 27
+    // }, { 
+    //     name: "Vadim",
+    //     age: 40
+    // }, {
+    //     name: "Britney",
+    //     age: 45
+    // }]
+
+    // db.collection('users').insertMany(users).then((result) =>{
+    //     console.log(result.insertedCount)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+
+    // ********Read********
 
     // db.collection('users').findOne({_id : new ObjectID("5fd2983cf979f023546352d8")}, (error,user) => {
     //     if(error){
@@ -48,6 +79,9 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //     } 
     //     console.log(tasks)
     // })
+
+
+    // ********Update********
 
     // const updatePromise = db.collection('users').updateOne({ 
     //     _id :new ObjectID("5fd290f135aebc0448945726")
@@ -87,6 +121,25 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     // }).then((result) =>{
         // console.log(result.modifiedCount)
     // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+
+    // ********Delete********
+    
+    // db.collection('users').deleteOne({
+    //     _id :new ObjectID("5fd290f135aebc0448945726")
+    // }).then((result) => {
+    //     console.log(result.deletedCount)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // db.collection('users').deleteMany({
+    //     age:25
+    // }).then((result) => {
+    //     console.log(result.deletedCount)
+    // }).catch((error) =>{
     //     console.log(error)
     // })
 })
