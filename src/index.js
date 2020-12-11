8f73c885fcbedf59236d4ecf66c0802709a384a1
Edenik <Edenik5@gmail.com>
2020-12-12 01:55:25 +0200

    Express Middleware

--- src/index.js
+++ src/index.js
@@ -7,8 +7,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+// app.use((req, res, next) => {
+//     if(req.method === 'GET'){
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
 
+app.use((req,res,next) => {
+    res.send('Site is currently down. Check back soon!')
+})
+
+app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
