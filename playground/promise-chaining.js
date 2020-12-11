16b55a5acec02fc0beccb5f78749294326687c36
Edenik <Edenik5@gmail.com>
2020-12-11 16:20:18 +0200

    Async/Await

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5fd3662835acf83a2c8d2c7c', { age: 1}).then((user) => {
-    console.log(user)
-    return User.countDocuments({age:1})
-}).then((result) =>{
-    console.log(`There is ${result} users with the age of 1`)
-}).catch((e) => {
-    console.log(e)
+// User.findByIdAndUpdate('5fd3662835acf83a2c8d2c7c', { age: 1}).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({age:1})
+// }).then((result) =>{
+//     console.log(`There is ${result} users with the age of 1`)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id,age) => {
+    const user = await User.findByIdAndUpdate(id, {age})
+    const count = await User.countDocuments({age})
+    return {user,count}
+}
+
+updateAgeAndCount('5fd36cd19a312d0de09eccfe', 1).then((result) => {
+    console.log(result)
+}).catch((error) => {
+    console.log(error)
 })
--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -2,11 +2,23 @@ require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
 
-Task.findByIdAndDelete('5fd3619d4e580e41b0b5d1c3').then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed:false})
-}).then((result) => {
-    console.log(`There are ${result} incompleted tasks`)
-}).catch((e) => {
-    console.log(e)
+// Task.findByIdAndDelete('5fd3619d4e580e41b0b5d1c3').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed:false})
+// }).then((result) => {
+//     console.log(`There are ${result} incompleted tasks`)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed:false})
+    return count
+}
+
+deleteTaskAndCount('5fd36edbe06aa11d189cb593').then((count) =>{
+    console.log('Count '+ count)
+}).catch((error) => {
+    console.log('Error: ' + error)
 })
