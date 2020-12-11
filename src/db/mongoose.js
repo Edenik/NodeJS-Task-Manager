f1a5f0cb8761c7a43cd55180971d71e88c3fdf4c
Edenik <Edenik5@gmail.com>
2020-12-11 14:07:14 +0200

    Creating a Mongoose Model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -16,13 +16,34 @@ const User = mongoose.model('User' ,{
     }
 })
 
-const me = new User({
-    name:"Alon",
-    age:27
+// const me = new User({
+//     name:"Alon",
+//     age:27
+// })
+
+// me.save().then((me) =>{
+//     console.log(me)
+// }).catch((error)=> {
+//     console.log('Error!' , error)
+// })
+
+const Task = mongoose.model('Task' , {
+    description : {
+        type:String
+    }, 
+    completed: {
+        type:Boolean
+    }
 })
 
-me.save().then((me) =>{
-    console.log(me)
-}).catch((error)=> {
-    console.log('Error!' , error)
+
+const newTask = new Task({
+    description : "Get a job!",
+    completed:false
+})
+
+newTask.save().then(() => {
+    console.log(newTask)
+}).catch((error) => {
+    console.log('Error!', error)
 })
