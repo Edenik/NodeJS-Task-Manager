ad30783489f816673763a5e250d8805af7633912
Edenik <Edenik5@gmail.com>
2020-12-12 20:57:02 +0200

    Sending Welcome and Cancelation Emails

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,13 +3,35 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-sgMail.send({
-    from:process.env.EMAIL,
-    to:process.env.EMAIL,
-    subject:'This is my first creation',
-    text:'I hope this one actually get to you.'
-}).then(() => {
-    console.log('Email sent')
-}).catch((e) => {
-    console.log('Error:' , e)
-})
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        from: process.env.EMAIL,
+        to: email,
+        subject: 'Welcome To Task Manager App!',
+        text: `Hi ${name}, Welcome to the Task Manager app. Let us know how you get along with the app!`
+    }).then(() => {
+        console.log('Email sent')
+    }).catch((e) => {
+        console.log('Error:', e)
+    })
+}
+
+const sendCencelationEmail = (email, name) => {
+    sgMail.send({
+        from: process.env.EMAIL,
+        to: email,
+        subject: 'It hurts to say goodbye!',
+        text: `Hi ${name}, we are sorry to hear that you deleted your account at Task Manager App.
+        We hope to see you back soon.`
+    }).then(() => {
+        console.log('Email sent')
+    }).catch((e) => {
+        console.log('Error:', e)
+    })
+}
+
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCencelationEmail
+}
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const sharp = require('sharp')
 const multer = require('multer')
-
+const { sendWelcomeEmail, sendCencelationEmail } = require('../emails/account')
 const router = new express.Router()
 
 
@@ -15,6 +15,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
 
         res.status(201).send({ user, token })
@@ -87,8 +88,10 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
+        sendCencelationEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) {
+        console.log(e)
         res.status(500).send()
     }
 })
