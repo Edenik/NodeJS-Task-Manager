23071c4ac452d1f8ccac56fa7bfb8d23ef14603e
Edenik <Edenik5@gmail.com>
2020-12-12 02:14:57 +0200

    Accepting Authentication Tokens

--- src/index.js
+++ src/index.js
@@ -7,18 +7,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if(req.method === 'GET'){
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-app.use((req,res,next) => {
-    res.send('Site is currently down. Check back soon!')
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 
 const router = new express.Router()
 
@@ -32,13 +33,8 @@ router.post('/users/login', async (req, res) => {
 })
 
 // Get all users
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 // Get user by id
